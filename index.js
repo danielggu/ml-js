4a551d20e84282d9713c1921804dd35f8efdfdda
Daniel García <danielggu@inditex.com>
2019-05-13 19:50:24 +0200

    Add random input data getter

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+const mathUtils = require('./lib/math-utils');
 const KMeansClassifier = require('./lib/k-means');
 
-(async () => {
-	const inputData = [
-		[1, 2],
-		[0, 0],
-		[1, 1],
-		[5, 5],
-		[9, 8],
-		[6, 7],
-		[8, 8]
-	];
+const getInputData = dimension => {
+	const samples = [];
+	for (let i = 0; i < 100; i ++) {
+		let sample = [];
+		for (let j = 0; j < dimension; j ++) {
+			sample.push(mathUtils.randomInteger(1, 50));
+		}
+		samples.push(sample);
+	}
 
+	return samples;
+};
+
+(async () => {
+	const inputData = await getInputData(2);
 	const test = await KMeansClassifier.run(inputData, 2);
 })();
